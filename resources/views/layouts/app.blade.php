842024b04d6580f58383ba7253af6b281f4af2de
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-07 17:27:30 +0100

    Added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <title>@yield('title', config('app.name', 'Laravel'))</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', config('app.name', 'Laravel'))</title>
 
 
     <!-- Fonts -->
