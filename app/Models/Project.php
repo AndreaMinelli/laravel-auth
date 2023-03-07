7272ebc03ce561c91edf6e2978a55cc0d46a62e8
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-07 17:06:43 +0100

    Added getter function

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected function getDescriptionAttribute($value)
+    {
+        return substr($value, 0, 40) . '...';
+    }
+
+    public function getProjectLink()
+    {
+        $link = $this->project_link;
+        if (strlen($link) > 20) {
+            $link =  substr($this->project_link, 0, 20) . '...';
+        }
+        return $link;
+    }
 }
