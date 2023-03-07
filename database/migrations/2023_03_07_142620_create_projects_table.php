715f6e38e23c229b6bed694f50a88cdd1108b72c
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-07 15:41:00 +0100

    Created projects_table column

--- database/migrations/2023_03_07_142620_create_projects_table.php
+++ database/migrations/2023_03_07_142620_create_projects_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->string('project_img')->nullable();
+            $table->text('description')->nullable();
+            $table->string('project_link')->nullable();
             $table->timestamps();
         });
     }
