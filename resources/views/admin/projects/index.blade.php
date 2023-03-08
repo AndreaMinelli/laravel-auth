f911c6895bc3f04d66bd834dc2e0c235b49d1e6f
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-08 14:49:25 +0100

    Added projects.create route

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,8 +3,10 @@
 @section('title', 'Projects')
 
 @section('content')
-
     <h1 class="text-center my-4">I miei progetti</h1>
+    <div class="text-end my-4">
+        <a class="btn btn-success btn-sm" href="{{ route('admin.projects.create') }}">Aggiungi progetto</a>
+    </div>
     <table class="table table-striped align-middle">
         <thead>
             <tr>
@@ -25,6 +27,7 @@
                     <td class="">
                         <a class="btn btn-primary btn-sm"
                             href="{{ route('admin.projects.show', $project->id) }}">Visualiza</a>
+
                     </td>
                 </tr>
             @empty
