f5c720e0741d5eaa2f0a042caab6eb8c6285008e
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-08 15:32:17 +0100

    Added margin to alerts

--- resources/views/includes/alerts/session.blade.php
+++ resources/views/includes/alerts/session.blade.php
@@ -1,5 +1,5 @@
 @if (session('msg'))
-    <div class="alert alert-{{ session('type') }}" role="alert">
+    <div class="alert alert-{{ session('type') }} mt-5" role="alert">
         {{ session('msg') }}
     </div>
 @endif
