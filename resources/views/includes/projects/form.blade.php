ec1872e6ef27e5128cbb5b86ecdd1909a45021cd
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-08 16:13:12 +0100

    Added error message under form's field

--- resources/views/includes/projects/form.blade.php
+++ resources/views/includes/projects/form.blade.php
@@ -8,22 +8,44 @@
 @csrf
 <div class="col-4">
     <label for="name" class="form-label">Nome Progetto:</label>
-    <input type="text" class="form-control" id="name" name="name" placeholder="Inserisci nome progetto"
-        value="{{ old('name', $project->name) }}" required>
+    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name"
+        placeholder="Inserisci nome progetto" value="{{ old('name', $project->name) }}" required>
+    @error('name')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @enderror
 </div>
 <div class="col-4">
     <label for="project_img" class="form-label">Immagine:</label>
-    <input type="url" class="form-control" id="project_img" name="project_img"
-        value="{{ old('project_img', $project->project_img) }}" placeholder="Inserisci url dell'immagine" required>
+    <input type="url" class="form-control @error('project_img') is-invalid @enderror" id="project_img"
+        name="project_img" value="{{ old('project_img', $project->project_img) }}"
+        placeholder="Inserisci url dell'immagine" required>
+    @error('project_img')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @enderror
 </div>
 <div class="col-4">
     <label for="project_link" class="form-label">Git Hub Link:</label>
-    <input type="url" class="form-control" id="project_link" name="project_link"
-        value="{{ old('project_link', $project->project_link) }}" placeholder="Inserisci url del link" required>
+    <input type="url" class="form-control @error('project_link') is-invalid @enderror" id="project_link"
+        name="project_link" value="{{ old('project_link', $project->project_link) }}"
+        placeholder="Inserisci url del link" required>
+    @error('project_link')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @enderror
 </div>
 <div class="col-12">
     <label for="description" class="form-label">Descrizione:</label>
-    <textarea class="form-control" id="description" name="description" required>{{ old('description', $project->description) }}</textarea>
+    <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" required>{{ old('description', $project->description) }}</textarea>
+    @error('description')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @enderror
 </div>
 <div class="text-end mt-5">
     <button class="btn btn-success">Salva</button>
