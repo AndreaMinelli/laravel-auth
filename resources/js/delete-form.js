726b338b9ac8c115fdbad4d84b60959c1bb87bb0
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-08 17:14:42 +0100

    Fixed delete-form.js error

--- resources/js/delete-form.js
+++ resources/js/delete-form.js
@@ -2,7 +2,7 @@ const forms = document.querySelectorAll(".delete-form");
 
 forms.forEach((form) => {
     form.addEventListener("submit", (e) => {
-        e.preventDefault;
+        e.preventDefault();
         const confirmed = confirm("Sei sicuro di voler eliminare?");
         if (confirmed) form.submit();
     });
